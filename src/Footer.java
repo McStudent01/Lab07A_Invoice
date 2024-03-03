d4c9c8d6089d3c243293e2afdd55e29431000626
McStudent01 <mcmillcy@mail.uc.edu>
2024-03-03 13:03:42 -0600

    added logic to Footer.java

--- src/Footer.java
+++ src/Footer.java
@@ -1,2 +1,17 @@
-public class Footer {
+public class Footer
+{
+    private AmountDue amountDue;
+
+    public Footer(AmountDue amountDue)
+    {
+        this.amountDue = amountDue;
+    }
+
+    public AmountDue getAmountDue() {
+        return amountDue;
+    }
+
+    public void setAmountDue(AmountDue amountDue) {
+        this.amountDue = amountDue;
+    }
 }
