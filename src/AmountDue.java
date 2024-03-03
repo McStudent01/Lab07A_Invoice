543062b89e887000212b04811b943bf79c19c884
McStudent01 <mcmillcy@mail.uc.edu>
2024-03-03 13:03:26 -0600

    added logic to AmountDue.java

--- src/AmountDue.java
+++ src/AmountDue.java
@@ -1,2 +1,17 @@
-public class AmountDue {
+public class AmountDue
+{
+    private double totalAmount;
+
+    public AmountDue(double totalAmount)
+    {
+        this.totalAmount = totalAmount;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
 }
