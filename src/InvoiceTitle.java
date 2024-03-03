317c4b8ea8d6e8b8f4799dc6a514208a115dfcfe
McStudent01 <mcmillcy@mail.uc.edu>
2024-03-03 13:01:46 -0600

    added logic to InvoiceTitle.java

--- src/InvoiceTitle.java
+++ src/InvoiceTitle.java
@@ -1,2 +1,17 @@
-public class InvoiceTitle {
+public class InvoiceTitle
+{
+    private String title;
+
+    public InvoiceTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
