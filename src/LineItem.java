14308b0634e9d29a3a14c407e6a036117291b3cc
McStudent01 <mcmillcy@mail.uc.edu>
2024-03-03 13:04:39 -0600

    added logic to LineItem.java and LineItemsTable.java

--- src/LineItem.java
+++ src/LineItem.java
@@ -1,2 +1,42 @@
-public class LineItem {
+public class LineItem
+{
+    private String description;
+    private int quantity;
+    private double price;
+
+    public LineItem(String description, int quantity, double price)
+    {
+        this.description = description;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double calculateTotal()
+    {
+        return quantity * price;
+    }
 }
--- src/LineItemsTable.java
+++ src/LineItemsTable.java
@@ -1,2 +1,27 @@
-public class LineItemsTable {
+import java.util.ArrayList;
+import java.util.List;
+
+public class LineItemsTable
+{
+    private List<LineItem> lineItems;
+
+    public LineItemsTable()
+    {
+        this.lineItems = new ArrayList<>();
+    }
+
+    public void addLineItem(LineItem lineItem)
+    {
+        lineItems.add(lineItem);
+    }
+
+    public double calculateTotal()
+    {
+        double total = 0;
+        for (LineItem item : lineItems)
+        {
+            total += item.calculateTotal();
+        }
+        return total;
+    }
 }
